bbdd90a2e27d78f54febc5dfd2ffd9f9552304b4
pauladocce-lachy <pdocceweb@gmail.com>
2026-07-29 19:12:47 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// ==========================
+// Paula Docce
+// script.js
+// ==========================
+
+// Změna navigace při scrollování
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 50) {
+
+        header.classList.add("scrolled");
+
+    } else {
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
+
+
+
+// Animace sekcí
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+}, {
+
+    threshold: 0.15
+
+});
+
+sections.forEach(section => {
+
+    observer.observe(section);
+
+});
