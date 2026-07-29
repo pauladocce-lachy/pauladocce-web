45f36b1ce23df99d55222103ba2e4627bac8d3c3
pauladocce-lachy <pdocceweb@gmail.com>
2026-07-29 23:30:17 +0200

    Update dashboard.js

--- admin/js/dashboard.js
+++ admin/js/dashboard.js
@@ -5,3 +5,19 @@ if (!data.session) {
     window.location.href = "index.html";
 
 }
+
+const logout = document.getElementById("logoutBtn");
+
+if (logout) {
+
+    logout.addEventListener("click", async (e) => {
+
+        e.preventDefault();
+
+        await db.auth.signOut();
+
+        window.location.href = "index.html";
+
+    });
+
+}
