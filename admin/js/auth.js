c8337b90d998ec3ac7785db1fd7bf3fd63929744
pauladocce-lachy <pdocceweb@gmail.com>
2026-07-29 23:29:26 +0200

    Update auth.js

--- admin/js/auth.js
+++ admin/js/auth.js
@@ -1,1 +1,28 @@
+const form = document.getElementById("loginForm");
 
+if (form) {
+
+    form.addEventListener("submit", async (e) => {
+
+        e.preventDefault();
+
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        const { error } = await db.auth.signInWithPassword({
+            email,
+            password
+        });
+
+        if (error) {
+
+            alert("Neplatný e-mail nebo heslo.");
+            return;
+
+        }
+
+        window.location.href = "dashboard.html";
+
+    });
+
+}
