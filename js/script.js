ae7b8d41917b835317814262cd9584479ae4dac7
pauladocce-lachy <pdocceweb@gmail.com>
2026-07-30 15:15:32 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -188,3 +188,27 @@ window.addEventListener("scroll", () => {
 });
 
 });
+
+// =========================================
+// Mobile menu
+// =========================================
+
+const menuToggle = document.getElementById("menuToggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+    });
+
+});
